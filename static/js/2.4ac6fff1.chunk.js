f7676483a09ba67d269dd2b6cd39692534e4204b
Nenad Radovanovic <nesaplay@gmail.com>
2018-03-25 18:45:02 +0200

    Updates

--- static/js/2.4ac6fff1.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{896:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(0),l=n.n(a),r=n(137),c=n(427),o=function(){return l.a.createElement(c.c,null,l.a.createElement("div",null,l.a.createElement("h1",null,"Psst! You shouldn't be here!"),l.a.createElement("p",null,"Go back ",l.a.createElement(r.b,{to:"/"},"HOME")," and search for more repositories.")))};t.default=o}});
-//# sourceMappingURL=2.4ac6fff1.chunk.js.map
